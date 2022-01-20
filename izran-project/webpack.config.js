e6dd1fff5ba5c1dd56428164bc90add9dd3abb62
rokussadee <rokus.sadee@student.ehb.be>
2022-01-20 15:47:33 +0100

    Aanmaak docs, html placement
    dist renamed to docs, html's correctly placed (in docs folder instead of src), multiple js files minified inside webpack

--- izran-project/webpack.config.js
+++ izran-project/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  // output: {
+  //   filename: 'main.js',
+  //   path: path.resolve(__dirname, 'docs'),
+  // },
+  entry: {
+    index: './src/index.js',
+    stories: './src/stories.js',
+  }, 
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'docs'),
+      filename: '[name].js'
   },
   mode: 'development',
   watch: true
