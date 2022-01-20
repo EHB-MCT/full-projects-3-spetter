76d77b93fdc3683481c30a4f07381c042d0fe5d0
CosimaLagae <cosima.lagae@student.ehb.be>
2022-01-20 21:31:04 +0100

    Story page made
    header, body are made

--- izran-project/docs/story.js
+++ izran-project/docs/story.js
@@ -7,16 +7,15 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/stories.js":
-/*!************************!*\
-  !*** ./src/stories.js ***!
-  \************************/
+/***/ "./src/story.js":
+/*!**********************!*\
+  !*** ./src/story.js ***!
+  \**********************/
 /***/ (() => {
 
-eval("\r\n\r\nwindow.onload = function() {\r\n    console.log('stories js') \r\n};\n\n//# sourceURL=webpack://izran-project/./src/stories.js?");
+eval("\n\n//# sourceURL=webpack://izran-project/./src/story.js?");
 
 /***/ })
 
@@ -27,7 +26,7 @@ eval("\r\n\r\nwindow.onload = function() {\r\n    console.log('stories js') \r\n
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/stories.js"]();
+/******/ 	__webpack_modules__["./src/story.js"]();
 /******/ 	
 /******/ })()
 ;
