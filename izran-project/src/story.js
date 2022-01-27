61fe038221912dcb60b1d30c3e5c46c5fd99e429
Tuurmeeus <50370038+Tuurmeeus@users.noreply.github.com>
2022-01-27 22:46:16 +0100

    deleted second fetch call

--- izran-project/src/story.js
+++ izran-project/src/story.js
@@ -5,23 +5,7 @@ window.onload = () => {
     let storyId = sessionStorage.getItem("storyId");
     console.log(storyId);
 
-    async function runTest(){
-      const resp = await fetch(`http://localhost:4000/api/stories-content/${storyId}`);
-      const data = await resp.json();
-
-      console.log(data);
-        
-
-        
-      
-      data.forEach(element => {
-        
-       });
-       
-  
-    }
-  
-    runTest();
+    
     
   }
 
